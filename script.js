af988cb2b95468734f7200480498abe81a4d8839
Joshua Morris <25271173+jmorris1990@users.noreply.github.com>
2018-12-03 14:33:42 -0500

    fixed game loop, keyboard control variable scoping, removed move function

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ let fruit;
 let fruitX;
 let fruitY;
 
+ // character's movement
+ let leftArrowPressed = false;
+ let rightArrowPressed = false;
+ let downArrowPressed = false;
+ let upArrowPressed = false;
 
 //initialize game
 window.onload = function () {
@@ -18,13 +23,6 @@ window.onload = function () {
     setInterval(loop, 1000 / 60)
 };
 
-//game loop
-let loop = () => {
-    drawCanvas();
-    drawPlayer();
-    drawFruit();
-    controls();
-}
 
 let drawCanvas = () => {
     ctx.fillStyle = 'grey'
@@ -43,4 +41,48 @@ let drawFruit = () => {
     fruitY = 50;
     ctx.fillStyle = 'yellow';
     ctx.fillRect(fruitX, fruitY, 10, 10);
+}
+
+
+
+function control() {
+   
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+
+    function keyDownHandler(e) {
+        if (e.keyCode == 39) {
+            rightArrowPressed = true;
+            console.log(rightArrowPressed)
+        } else if (e.keyCode == 37) {
+            leftArrowPressed = true;
+            console.log(leftArrowPressed)
+        } else if (e.keyCode == 38) {
+            upArrowPressed = true;
+            console.log(upArrowPressed)
+        } else if (e.keyCode == 40) {
+            downArrowPressed = true;
+            console.log(downArrowPressed)
+        }
+    }
+
+    function keyUpHandler(e) {
+        if (e.keyCode == 39) {
+            rightArrowPressed = false;
+        } else if (e.keyCode == 37) {
+            leftArrowPressed = false;
+        } else if (e.keyCode == 38) {
+            upArrowPressed = false;
+        } else if (e.keyCode == 40) {
+            downArrowPressed = false;
+        }
+    }
+}
+
+//game loop
+let loop = () => {
+    drawCanvas();
+    drawPlayer();
+    drawFruit();
+    control();
 }
